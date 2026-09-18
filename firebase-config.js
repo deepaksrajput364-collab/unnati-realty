6e13b5aad3d1de3d58d907d5f2ab5130030ebba6
deepaksrajput364-collab <deepaksrajput364@gmail.com>
2026-09-19 02:31:10 +0530

    Add Firebase configuration for ReaLynk app

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// ReaLynk Firebase Web App configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4Bm9-yRhOLTyCYoUWVRkawC3z7FlJiMg",
+  authDomain: "realynk8.firebaseapp.com",
+  projectId: "realynk8",
+  storageBucket: "realynk8.firebasestorage.app",
+  messagingSenderId: "109714308000",
+  appId: "1:109714308000:web:b063c17cf9a141decec2c5",
+  measurementId: "G-JR1MGBLBHG"
+};
